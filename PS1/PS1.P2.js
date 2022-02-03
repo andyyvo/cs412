7d9d0c28da18df9b4d7d78f0a4c23245e19a691e
Andy Vo <andyvo@dhcp-wifi-8021x-155-41-35-136.bu.edu>
2022-02-03 00:27:55 -0500

    finished PS1

--- PS1/PS1.P2.js
+++ PS1/PS1.P2.js
@@ -18,3 +18,61 @@
  * Execute the returned function to evaluate and print each expression.
  */
 
+// expressions to calculate
+const expr1 = '4+2';
+const expr2 = '5*7';
+const expr3 = '6-1';
+const expr4 = '9/2';
+const expr5 = '2^8';
+
+/**
+ *
+ * @param expr - expression to be evaluated
+ * @returns {function(*, *)} - function that implements the operator to make calculation
+ */
+const evaluate = expr => {
+    if (expr.includes('+')) {
+        // addition
+        return (left, right) => left + right;
+    } else if (expr.includes('*')) {
+        // multiplication
+        return (left, right) => left * right;
+    } else if (expr.includes('-')) {
+        // subtraction
+        return (left, right) => left - right;
+    } else if (expr.includes('/')) {
+        // division
+        return (left, right) => left / right;
+    } else if (expr.includes('^')) {
+        // exponentiation
+        return (left, right) => left ** right;
+    } else {
+        // modulus not requested in problem
+        console.log('Invalid expression.')
+    }
+}
+
+/**
+ *
+ * @param expr - expression to be evaluated
+ * @param operCallback - callback function that takes in the evaluate function
+ * @returns {*} - uses callback with parsed values to run the evaluate function
+ */
+const calculate = (expr, operCallback) => {
+    let exprList = expr.split('');
+    let [leftNum, operator, rightNum] = [exprList[0], exprList[1], exprList[2]];
+    return operCallback(parseInt(leftNum), parseInt(rightNum));
+}
+
+/**
+ *
+ * @param expr - expression to be evaluated
+ * @returns {*} - calculated value
+ */
+const operate = expr => calculate(expr, evaluate(expr));
+
+console.log(`${expr1} = ${operate(expr1)}`);
+console.log(`${expr2} = ${operate(expr2)}`);
+console.log(`${expr3} = ${operate(expr3)}`);
+console.log(`${expr4} = ${operate(expr4)}`);
+console.log(`${expr5} = ${operate(expr5)}`);
---
+++ PS1/PS1.P3.js
@@ -0,0 +1,68 @@
+/**
+ * Andy Vo
+ *
+ * Problem 3
+ *
+ * Write a function that accepts two input parameters: a string, and a decorator function. The function should execute
+ * the passed decorator function on the passed string and return the result.
+ *
+ * Next, write two expressions that call this function.
+ *
+ * For the first, pass the string
+ * 'supercalifragilisticexpialidocious' and a lambda (unnamed) function that returns an array contain fragments of the
+ * input string broken on the character 'c'. For the input string 'supercalifragilisticexpialidocious', you should get
+ * ['super', 'califragilisti', 'cexpialido', 'cious']
+ *
+ * For the second, pass the string 'supercalifragilisticexpialidocious' and a lambda function that replaces all the
+ * 'a' characters with 'A' characters. Return an object that contains the original string, the modified string, the
+ * total number of As in the modified string, and the overall length of the modified string:
+ *
+ * {
+ *     originalString: XXX,
+ *     modifiedString: XXX,
+ *     numberReplaced: XXX,
+ *     length: XXX,
+ * }
+ */
+
+// easier reference to the word
+const superStr = 'supercalifragilisticexpialidocious';
+
+/**
+ *
+ * @param str - string
+ * @param decor - decorator function
+ */
+const strDecFunction = (str, decor) => decor(str);
+
+/**
+ * Function that passed in superStr and lambda function to return array breaking up string by 'c'.
+ * Using a callback, delimiter is kept within the array.
+ */
+const expr1 = strDecFunction(superStr, splitter => {
+    // source on keeping delimiter for regex: https://medium.com/@shemar.gordon32/how-to-split-and-keep-the-delimiter-s-d433fb697c65
+    return superStr.split(/(?=['c'])/g);
+});
+
+console.log(expr1);
+
+/**
+ * Function that passed in superStr and lambda function to return object of original string, modified string
+ * of a -> A, number of As, and overall length of modified string.
+ */
+const expr2 = strDecFunction(superStr, capitalize => {
+    let modified = superStr.replace(/a/g, 'A'); // not 'a', /a/g is global rather than first
+    let replaced = modified.split('A').length - 1 // minus 1 bc the first element of arr is before 'A'
+    let lengthSt = superStr.length;
+
+    const result = {
+        'originalString': superStr,
+        'modifiedString': modified,
+        'numberReplaced': replaced,
+        'length': lengthSt,
+    }
+
+    return result;
+});
+
+console.log(expr2);
